0223ec6125efc5e70bcf7567d038ef4fdb430b7b
Luis Mesajil <lhmesajil@gmail.com>
2024-11-16 21:16:02 -0500

    feat(form.component): Add component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,13 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
 import { RouterOutlet } from '@angular/router';
+import { FormComponent } from './form/form.component';
 import { LogoComponent } from './logo/logo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    LogoComponent,
-    CommonModule,
-    MatFormFieldModule,
-    FormsModule,
-    MatInputModule,
-    MatSelectModule,
-    MatCheckboxModule,
-    MatButtonModule,
-  ],
+  imports: [RouterOutlet, LogoComponent, FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'My form';
-
-  // Modelo para capturar los valores del formulario
-  formData = {
-    textInput: '',
-    selectedOption: '',
-    isChecked: false,
-  };
-
-  // Opciones para el select
-  options = ['Perú', 'Chile', 'Argentina'];
-
-  // Método para guardar y mostrar en la consola los valores del formulario
-  saveForm() {
-    console.log('Form Data:', this.formData);
-  }
-}
+export class AppComponent {}
